919192d9f3e824acc95ecf06cc10bc36f4da6eb7
Matteo Grellier <matteogrellier@gmail.com>
2023-02-20 22:14:19 +0100

    :construction: | Started 404 page creation

---
+++ src/views/404.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>404</h1>
+    <h1>PAGE NOT FOUND</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  background-color: rgb(250, 28, 65);
+}
+</style>
