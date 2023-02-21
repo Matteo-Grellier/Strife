29ca636a70591749ace4939c0569a608d930c2bf
matheoleger <71370253+matheoleger@users.noreply.github.com>
2023-02-21 09:55:29 +0100

    :lipstick: Added import for Montserrat

--- src/components/StylingExample.vue
+++ src/components/StylingExample.vue
@@ -6,7 +6,7 @@
     <button><img src="https://cdn-icons-png.flaticon.com/512/261/261987.png" width="30"/><span>Test button with icon</span></button>
     <input type="text" placeholder="Votre nom svp...">
     <textarea name="testtext" id="testtext" placeholder="Entre ton texte"></textarea>
-    <input type="textarea"><div v-for="index in 2" :key="index">
+    <div v-for="index in 2" :key="index">
         <h4>m.leger<span>✨</span><span class="date">• Aujourd'hui à 14h36</span></h4>
         <p>Ceci est mon premier message avec un <a>super lien</a> ! <b>Bold</b> <i>Italique</i> <b><i>Bold et italique</i></b></p>
     </div>
