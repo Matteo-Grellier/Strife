4b2d62d5a20fa83ddc99958b29b1d9eab143caf9
LBROCHARD <73283488+LBROCHARD@users.noreply.github.com>
2023-02-28 16:22:29 +0100

    :construction: added the base of the ChanelBar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router';
+import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
   <header>
-
+    <h1>Strife is life ! Strife, say goodbye to your wife !</h1>
+    <p>(for the sake of the rhyme, every user, male or female, is advised to have a wife)</p>
     <nav>
       <RouterLink to="/login">Login</RouterLink>
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/ff">random</RouterLink>
-    </nav>
+    </nav> 
   </header>
 
   <main>
@@ -17,4 +18,5 @@ import { RouterLink, RouterView } from 'vue-router';
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+    import ChannelSidebar from "../components/ChanelSidebar.vue";
 </script>
 
 <template>
     <main>
       <h1>Strife is life ! Strife, say goodbye to your wife !</h1>
       <p>(for the sake of the rhyme, every user, male or female, is advised to have a wife)</p>
+      <ChannelSidebar serverName="yes" serverImg="yes"/>
     </main>
 </template>
