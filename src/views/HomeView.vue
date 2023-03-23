f64955f6e93ac97758f8647a4577ab190addbf6a
matheoleger <71370253+matheoleger@users.noreply.github.com>
2023-03-23 23:42:20 +0100

    :adhesive_bandage: I put the correct content of the file for push on dev

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,8 @@
 <script setup lang="ts">
-    import ChannelSidebar from "../components/ChanelSidebar.vue";
-    import ChannelContent from "@/components/ChannelContent.vue";
 </script>
 
 <template>
     <main>
-        <ChannelSidebar/>
-        <ChannelContent :channelId="1"/>
-        <ChannelSidebar/>
+        home 
     </main>
 </template>
-<style>
-    main {
-        display: flex;
-        flex-grow: 2;
-        width: 100vw;
-    }
-</style>
